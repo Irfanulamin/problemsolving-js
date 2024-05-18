41d7380fd5c41d948609898a7fba9412799d54f1
Irfanulamin <irfanulamin10@gmail.com>
2024-05-18 17:24:35 +0600

    <problem-2> problem 1 was solved and was working fine

---
+++ problem-2.js
@@ -0,0 +1,8 @@
+// Create An Array Of Objects Representing Books With Properties Like Title, Author, And Year.
+// Write A Function That Takes The Array And Returns A New Array With Only The Book Titles.Print The Result.
+
+function getTitlesOfBooks(arrOfBooks) {
+  const titlesOfBooks = arrOfBooks.map((book) => book.title);
+
+  return titlesOfBooks;
+}
